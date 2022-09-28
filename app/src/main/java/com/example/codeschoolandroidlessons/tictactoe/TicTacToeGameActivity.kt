c27457fd1fab8de36f2d65b64d6d287b1774962b
karensukiasyan <Karensuqiasyan7@gmail.com>
2022-09-28 15:11:37 +0400

    some change

--- app/src/main/java/com/example/codeschoolandroidlessons/tictactoe/TicTacToeGameActivity.kt
+++ app/src/main/java/com/example/codeschoolandroidlessons/tictactoe/TicTacToeGameActivity.kt
@@ -2,6 +2,7 @@ package com.example.codeschoolandroidlessons.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.example.codeschoolandroidlessons.databinding.ActivityMainBinding
 import com.example.codeschoolandroidlessons.databinding.ActivityTicTacToeGameBinding
 
@@ -12,11 +13,14 @@ class TicTacToeGameActivity : AppCompatActivity() {
     var playerTurn = 1
     var totalSelectedBoxes = 1
 
+
     private lateinit var binding: ActivityTicTacToeGameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTicTacToeGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.playerOneLayout.setOnClickListener {  }
     }
 }

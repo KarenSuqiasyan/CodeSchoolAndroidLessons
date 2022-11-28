98dd65a395935bca50163b13a61a10f7ff05a9f2
karensukiasyan <karensuqiasyan@gmai.com>
2022-11-28 18:59:29 +0400

    guardian internet check change

--- app/src/main/java/com/example/codeschoolandroidlessons/guardian/util/Constants.kt
+++ app/src/main/java/com/example/codeschoolandroidlessons/guardian/util/Constants.kt
@@ -4,5 +4,4 @@ object Constants {
 
     const val API_KEY = "c4e8c6de-cdfa-42a9-a87d-ef9835041f5a"
     const val BASE_URL = "https://content.guardianapis.com/"
-    const val QUERY_PAGE_SIZE = 1
 }
